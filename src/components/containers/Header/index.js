11b00b3ce49b00de370c89764525f9d17240aa5d
Jeremy Dryden <jeremy.dryden@gmail.com>
2023-10-20 23:48:18 -0400

    trying to get content on an image

--- src/components/containers/Header/index.js
+++ src/components/containers/Header/index.js
@@ -1,8 +1,20 @@
 import * as React from "react";
+import { StaticImage } from "gatsby-plugin-image";
+import * as styles from "./index.module.css";
 
 const Header = ({ siteTitle }) => (
-  <header>
-    <span>asdf</span>
+  <header className={styles.container}>
+    <StaticImage
+      className="backgroundImage"
+      layout="fullWidth"
+      alt=""
+      src="../../../images/backgroundLeft.png"
+      // src="@/images/backgroundLeft.png"
+      formats={["auto", "webp", "avif", "png"]}
+    />
+    <div className="content">
+      <h1>Portfolio</h1>
+    </div>
   </header>
 );
 
