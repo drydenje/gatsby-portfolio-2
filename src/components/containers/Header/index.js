b6c2778881c67937812f0bc6b04f52cd8d818551
Jeremy Dryden <jeremy.dryden@gmail.com>
2023-10-21 00:21:00 -0400

    opacity working, needs some positioning tweaks

--- src/components/containers/Header/index.js
+++ src/components/containers/Header/index.js
@@ -5,14 +5,15 @@ import * as styles from "./index.module.css";
 const Header = ({ siteTitle }) => (
   <header className={styles.container}>
     <StaticImage
-      className="backgroundImage"
+      className={styles.backgroundImage}
       layout="fullWidth"
+      loading="eager"
       alt=""
       src="../../../images/backgroundLeft.png"
       // src="@/images/backgroundLeft.png"
       formats={["auto", "webp", "avif", "png"]}
     />
-    <div className="content">
+    <div className={styles.content}>
       <h1>Portfolio</h1>
     </div>
   </header>
